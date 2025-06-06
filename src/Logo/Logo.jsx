bd0183e857bf2a3a16b963f5e0fdb8f6d3782b1a
priyanka <cpriyanka71m@gmail.com>
2025-06-06 15:48:55 +0530

    message

---
+++ src/Logo/Logo.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import LogoImage from "../../L-square-QTify/assets/logo.svg";
+
+export default function Logo() {
+  return <img src={LogoImage} alt="logo" width={67} />;
+}
