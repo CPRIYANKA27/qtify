4a598544d45b52524a4b8c6f1d5e43f326fa0159
Priyanka Chaudhari <cpriyanka71m@gmail.com>
2025-06-23 01:41:43 +0530

    New Functinality added

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = ({ data, page, songsData }) => {
         </div>
         <div className={styles.buttonWrapper}>
           <Button
-            text="GIVE FEEDBACK"
+            text="Give Feedback"
             eventHandler={{ event: "onClick", handler: handleClick }}
           />
         </div>
